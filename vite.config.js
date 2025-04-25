a1f263c4d73030a93ec69b4a583aa3b18e042836
SaimumIslam <saimumislam27@gmail.com>
2025-04-26 03:12:23 +0600

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `/`,
   server: {
     port: 3000,
     strictPort: true,
+  },
+  build: {
+    sourcemap: "hidden",
   },
 });
